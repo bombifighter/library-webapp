100dd5d63a981bd46e5096f0a1735071d56e7508
bombifighter <botond20@gmail.com>
2020-12-12 21:48:38 +0100

    Initialized Spring backend

---
+++ library-backend/src/main/java/com/botond/librarybackend/LibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.botond.librarybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryBackendApplication.class, args);
+	}
+
+}
---
+++ library-backend/src/test/java/com/botond/librarybackend/LibraryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botond.librarybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
